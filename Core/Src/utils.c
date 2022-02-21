5079d81e8d296e4fe555bf4f4ff1c582a5ab7842
hacknus <l_stoeckli@bluewin.ch>
2022-02-21 19:10:41 +0100

    implemented gyro in idle detect

--- Core/Src/utils.c
+++ Core/Src/utils.c
@@ -8,30 +8,45 @@
 #include "utils.h"
 #include <math.h>
 
-uint16_t idle_detect_buffer[IDLE_DETECT_LEN] = { 0.0 };
+uint16_t idle_detect_buffer_a[IDLE_DETECT_LEN] = { 0.0 };
+uint16_t idle_detect_buffer_g[IDLE_DETECT_LEN] = { 0.0 };
 uint16_t launch_detect_buffer[LAUNCH_DETECT_LEN] = { 0.0 };
 uint32_t empty_idle_buffer_counter = 0;
 uint32_t empty_launch_buffer_counter = 0;
 
 uint8_t device_is_idle(log_t *LOG, data_t *DATA, uint32_t len) {
-	// TODO: include gyro and baro
 	float a[3] = { 0.0 };
+	float g[3] = { 0.0 };
 
 	a[0] = ((float) DATA->accel_x) / LOG->accel_sens;
 	a[1] = ((float) DATA->accel_y) / LOG->accel_sens;
 	a[2] = ((float) DATA->accel_z) / LOG->accel_sens;
 
+	a[0] = ((float) DATA->gyro_x) / LOG->gyro_sens;
+	a[1] = ((float) DATA->gyro_y) / LOG->gyro_sens;
+	a[2] = ((float) DATA->gyro_z) / LOG->gyro_sens;
+
 	for (int i = 1; i < IDLE_DETECT_LEN; i++) {
-		idle_detect_buffer[i - 1] = idle_detect_buffer[i];
+		idle_detect_buffer_a[i - 1] = idle_detect_buffer_a[i];
+		idle_detect_buffer_g[i - 1] = idle_detect_buffer_g[i];
 	}
-	idle_detect_buffer[IDLE_DETECT_LEN - 1] = (uint16_t)(1000 * sqrtf(
+	idle_detect_buffer_a[IDLE_DETECT_LEN - 1] = (uint16_t)(1000 * sqrtf(
 			a[0] * a[0] + a[1] * a[1] + a[2] * a[2]));
+	idle_detect_buffer_g[IDLE_DETECT_LEN - 1] = (uint16_t)(1000 * sqrtf(
+			g[0] * g[0] + g[1] * g[1] + g[2] * g[2]));
 	float sum_a = 0.0;
+	float sum_g = 0.0;
 	for (int i = IDLE_DETECT_LEN - len; i < IDLE_DETECT_LEN; i++) {
-		sum_a += idle_detect_buffer[i];
+		sum_a += idle_detect_buffer_a[i];
+	}
+	for (int i = IDLE_DETECT_LEN - len; i < IDLE_DETECT_LEN; i++) {
+		sum_g += idle_detect_buffer_g[i];
 	}
 	sum_a /= (float) len;
 	sum_a /= 1000;
+	sum_g /= (float) len;
+	sum_g /= 1000;
+	printf("sum_g: %4.2f \n", sum_g);
 	if (empty_idle_buffer_counter <= IDLE_DETECT_LEN) empty_idle_buffer_counter ++;
 	if (empty_idle_buffer_counter < IDLE_DETECT_LEN) return 0;
 	if ((0.5 <= sum_a) && (sum_a <= 1.5))
